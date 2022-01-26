8285ac453074fae24671e9a04ef39361e5266174
Britton Robitzsch <brittonrobitzsch@gmail.com>
2022-01-25 19:56:31 -0700

    Update trankillity.h

--- config/trankillity.h
+++ config/trankillity.h
@@ -1,24 +1,24 @@
 #define _BASE 0
-#define _NUMB 1
-#define _FUNC 2
-#define _NAVI 3
-#define _SYST 4
+ #define _NUMB 1
+ #define _FUNC 2
+ #define _NAVI 3
+ #define _SYST 4
 
-#define NAV_ENT _NAVI ENTER
-#define NUM_BCK _NUMB BACKSPACE
+ #define NAV_ENT _NAVI ENTER
+ #define NUM_BCK _NUMB BACKSPACE
 
-#define X_SELA      LGUI(A)
-#define X_UNDO      LGUI(Z)
-#define X_CUT       LGUI(X)
-#define X_COPY      LGUI(C)
-#define X_PAST      LGUI(V)
-#define X_REDO      LGUI(Y)
+ #define X_SELA      LC(A)
+ #define X_UNDO      LC(Z)
+ #define X_CUT       LC(X)
+ #define X_COPY      LC(C)
+ #define X_PAST      LC(V)
+ #define X_REDO      LC(Y)
 
-#define X_A LGUI    A
-#define X_R LALT    R
-#define X_S LGUI   S
-#define X_T LSHFT   T
-#define X_N RSHFT   N
-#define X_E RCTRL   E
-#define X_I LALT    I
-#define X_O RGUI    O
+ #define X_A LGUI    A
+ #define X_R LALT    R
+ #define X_S LCTRL   S
+ #define X_T LSHFT   T
+ #define X_N RSHFT   N
+ #define X_E RCTRL   E
+ #define X_I LALT    I
+ #define X_O RGUI    O 
